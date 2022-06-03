ba76659d7831286dd26b229cf523bb5e0fbef334
Igor Yagodin <Igor.Yagodin@x5.ru>
2022-06-03 21:39:17 +0300

    1

--- reverse_string.go
+++ reverse_string.go
@@ -1,10 +1,12 @@
 package reverse_string
 
+import "github.com/kyokomi/emoji/v2"
+
 func ReverseString(input string) (output string) {
 	// solution goes here
 
 	for i := len(input) - 1; i >= 0; i-- {
 		output += string(input[i])
 	}
-	return output
+	return emoji.Sprint(output)
 }
